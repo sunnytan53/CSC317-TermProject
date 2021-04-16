ca48ac575c6721c365bc2c7e14fd0b55cdfdb11a
CiYuan53 <ciyuan53@gmail.com>
2021-04-15 20:18:56 -0700

    adpat the previous work into handlebars template

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,20 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('home');
+router.get('/', function (req, res, next) {
+    res.render('home',
+        { title: "Home"});
 });
 
 router.get('/login', (req, res, next) => {
-    res.render('login');
+    res.render('login',
+        { title: "Login" });
 });
 
 router.get('/registration', (req, res, next) => {
-    res.render('registration');
+    res.render('registration',
+        { title: "Registration", script: "registration.js" });
 });
 
 router.get('/postimage', (req, res, next) => {
-    res.render('postimage');
+    res.render('postimage',
+        { title: "Post image" });
+});
+
+router.get('/Imagepost', (req, res, next) => {
+    res.render('Imagepost');
 });
 
 module.exports = router;
